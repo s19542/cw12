6cc09fb98c2d64d1b56a1bba35177b8f34f9a973
Alisa Krsek <s19542@pjwstk.edu.pl>
2020-05-26 21:08:43 +0200

    Dodawanie kontrolera do aplikacji ASP.NET Core MVC

---
+++ cw12/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw12.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int numTimes)
+        {
+                                       //show
+            return HtmlEncoder.Default.Encode($"Hello {name}," +
+                $" NumTimes is: {numTimes}"); ;
+        }
+
+        
+
+
+
+    }
+}
