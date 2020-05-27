5c4c75044f7835ea0f31d320986fe757659ed518
Alisa Krsek <s19542@pjwstk.edu.pl>
2020-05-27 14:54:54 +0200

    Dodawanie widoku do aplikacji ASP.NET Core MVC

--- cw12/Controllers/HelloWorldController.cs
+++ cw12/Controllers/HelloWorldController.cs
@@ -10,20 +10,20 @@ namespace cw12.Controllers
     public class HelloWorldController : Controller
     {
         //GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        //GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int numTimes)
-        {
-                                       //show
-            return HtmlEncoder.Default.Encode($"Hello {name}," +
-                $" NumTimes is: {numTimes}"); ;
-        }
 
-        
+        //GET: https://{localhost}/HelloWorld/Welcome?name=Rick&numtimes=4
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
 
 
 
