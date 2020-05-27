e596a78869d33d885b3802af89c637a5fcf8b530
Alisa Krsek <s19542@pjwstk.edu.pl>
2020-05-27 21:06:17 +0200

    Dodanie wyszukiwania według gatunku

--- cw12/Models/Movie.cs
+++ cw12/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,9 +13,13 @@ namespace cw12.Models
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]//what will be shown
         [DataType(DataType.Date)]//show only date
         public DateTime ReleaseDate { get; set; }//user does not enter  time
         public string Genre { get; set; }
+
+        //jako decimal z dokładnością 18 i skalą 2
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
---
+++ cw12/Models/MovieGenreViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw12.Models
+{
+    public class MovieGenreViewModel
+    {
+    }
+}
